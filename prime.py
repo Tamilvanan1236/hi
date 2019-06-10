520bd37e399e2e0157fc544d476a728eccb92b11
Dharadharu <51207024+Dharadharu@users.noreply.github.com>
2019-06-10 15:58:37 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,9 @@
+n=int(input())
+if(n>0):
+    for i in range(2,n+1):
+        if(n%2!=0):
+              print("yes")
+              break;
+        else:
+            print("no")
+            break;
